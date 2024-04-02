67a55853ad2ca15bad7c630d7e8b8c4046514856
Mazen Chami <mazenchami@gmail.com>
2024-04-02 09:40:23 -0400

    Initial Commit

---
+++ Microphone Detector/Microphone_DetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Microphone_DetectorApp.swift
+//  Microphone Detector
+//
+//  Created by Mazen Chami on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Microphone_DetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
